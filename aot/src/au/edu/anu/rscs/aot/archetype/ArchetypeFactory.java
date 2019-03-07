b20d847443a82b145e6b0d8ac7f62d90fab1d4ba
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-03-07 14:17:27 +0100

    importing archetype - step 1

---
+++ aot/src/au/edu/anu/rscs/aot/archetype/ArchetypeFactory.java
@@ -0,0 +1,102 @@
+/**************************************************************************
+ *  AOT - Aspect-Oriented Thinking                                        *
+ *                                                                        *
+ *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
+ *       shayne.flint@anu.edu.au                                          *
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  AOT is a method to generate elaborate software code from a series of  *
+ *  independent domains of knowledge. It enables one to manage and        *
+ *  maintain software from explicit specifications that can be translated *
+ *  into any programming language.          							  *
+ **************************************************************************                                       
+ *  This file is part of AOT (Aspect-Oriented Thinking).                  *
+ *                                                                        *
+ *  AOT is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  AOT is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with UIT.  If not, see <https://www.gnu.org/licenses/gpl.html>. *
+ *                                                                        *
+ **************************************************************************/
+package au.edu.anu.rscs.aot.archetype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import fr.cnrs.iees.OmugiClassLoader;
+import fr.cnrs.iees.graph.TreeNode;
+import fr.cnrs.iees.graph.TreeNodeFactory;
+import fr.cnrs.iees.properties.SimplePropertyList;
+
+@SuppressWarnings("unchecked")
+public class ArchetypeFactory implements TreeNodeFactory {
+	
+	private static Map<String,Class<? extends TreeNode>> labelMappings = new HashMap<>();
+
+	static {
+		try {
+			ClassLoader cl = OmugiClassLoader.getClassLoader();
+			labelMappings.put("hasNode", (Class<? extends TreeNode>) 
+				Class.forName("au.edu.anu.rscs.aot.archetype.NodeSpec",false,cl));
+			labelMappings.put("hasProperty", (Class<? extends TreeNode>) 
+				Class.forName("au.edu.anu.rscs.aot.archetype.PropertySpec",false,cl));
+			labelMappings.put("hasEdge", (Class<? extends TreeNode>) 
+				Class.forName("au.edu.anu.rscs.aot.archetype.EdgeSpec",false,cl));
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public TreeNode makeTreeNode(TreeNode parent, String proposedId, SimplePropertyList properties) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode makeTreeNode(Class<? extends TreeNode> treeNodeClass, TreeNode parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode makeTreeNode(Class<? extends TreeNode> treeNodeClass, TreeNode parent,
+			SimplePropertyList properties) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode makeTreeNode(Class<? extends TreeNode> treeNodeClass, TreeNode parent, String proposedId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode makeTreeNode(Class<? extends TreeNode> treeNodeClass, TreeNode parent, String proposedId,
+			SimplePropertyList properties) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public Class<? extends TreeNode> treeNodeClass(String label) {
+		try {
+			return (Class<? extends TreeNode>) Class.forName("fr.cnrs.iees.graph.impl."+label,false,OmugiClassLoader.getClassLoader());
+		} catch (ClassNotFoundException e) {
+			return null;
+		}
+	}
+
+	
+}
---
+++ aot/src/au/edu/anu/rscs/aot/archetype/Archetypes.java
@@ -0,0 +1,66 @@
+/**************************************************************************
+ *  AOT - Aspect-Oriented Thinking                                        *
+ *                                                                        *
+ *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
+ *       shayne.flint@anu.edu.au                                          *
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  AOT is a method to generate elaborate software code from a series of  *
+ *  independent domains of knowledge. It enables one to manage and        *
+ *  maintain software from explicit specifications that can be translated *
+ *  into any programming language.          							  *
+ **************************************************************************                                       
+ *  This file is part of AOT (Aspect-Oriented Thinking).                  *
+ *                                                                        *
+ *  AOT is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  AOT is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with UIT.  If not, see <https://www.gnu.org/licenses/gpl.html>. *
+ *                                                                        *
+ **************************************************************************/
+package au.edu.anu.rscs.aot.archetype;
+
+import java.io.File;
+
+import fr.cnrs.iees.graph.Tree;
+import fr.cnrs.iees.graph.TreeNode;
+import fr.cnrs.iees.io.FileImporter;
+
+/**
+ * Just a test class for the moment -
+ * 
+ * @author Jacques Gignoux - 6 mars 2019
+ *
+ */
+public class Archetypes {
+	
+	private Tree<? extends TreeNode> archetypeArchetype = null;
+	
+	@SuppressWarnings("unchecked")
+	public Archetypes() {
+		super();
+		String archetypefile = System.getProperty("user.dir") // <home dir>/<eclipse workspace>/<project>
+			+ File.separator + "src" 
+			+ File.separator + this.getClass().getPackage().getName().replace('.',File.separatorChar) 
+			+ File.separator + "ArchetypeArchetype.ugt";
+		File file = new File(archetypefile);
+		FileImporter fi = new FileImporter(file);
+		archetypeArchetype  = (Tree<? extends TreeNode>) fi.getGraph();
+		for (TreeNode tn:archetypeArchetype.nodes())
+			System.out.println(tn.toDetailedString());
+	}
+	
+	public void checkArchetype(Tree<? extends TreeNode> archetype) {
+		
+	}
+
+}
