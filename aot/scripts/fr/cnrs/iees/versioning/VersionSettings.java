094feed116c1995b7abe33ee712fce2ed2436599
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-09-04 12:45:48 +0200

    fixing dependency versions

--- aot/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ aot/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -45,9 +45,9 @@ public class VersionSettings {
 	 * 
 	 */
 	protected static String[][] DEPS = { 
-		{"fr.ens.biologie", "generics", "[0.1.1,)", null},
-		{"au.edu.anu.rscs.aot", "omugi", "[0.2.1,)", null},
-		{"au.edu.anu.rscs.aot", "qgraph", "[0.1.1,)", null}
+		{"fr.ens.biologie", "generics", "[0.1.10,)", null},
+		{"au.edu.anu.rscs.aot", "omugi", "[0.2.10,)", null},
+		{"au.edu.anu.rscs.aot", "qgraph", "[0.1.2,)", null}
 	};
 	
 	/** The name of the main class to put in the jar manifest, if any. This enables users to
