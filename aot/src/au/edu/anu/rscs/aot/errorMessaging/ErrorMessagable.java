494195636fc58f10c6217a7716afa05551cac762
Ian Davies <ian.davies@anu.edu.au>
2019-11-26 13:39:23 +1100

    comment

--- aot/src/au/edu/anu/rscs/aot/errorMessaging/ErrorMessagable.java
+++ aot/src/au/edu/anu/rscs/aot/errorMessaging/ErrorMessagable.java
@@ -34,43 +34,28 @@ package au.edu.anu.rscs.aot.errorMessaging;
  * Message purpose: These messages inform the user as to what steps to take to
  * get a model constructed and running. I think it is no exaggeration to say
  * that the success of tw, measured as uptake, depends largely on how easy these
- * messages are to interpret. But there are different types of users and its
- * easy to fall into the trap of writing messages that are easy for us to write
- * (query not satisfied msgs) but not immediately obvious what this means in a
- * different context. It's the same problem with writing exceptions - the
- * message depends on context - which changes as errors bubble up through the
- * system. Yet as context changes, its important to preserve and pass on data as
- * the error bubbles up into new contexts.
- * 
- * In a sense, our 'errors' are not always really errors but instructions.
- * 
- * So what to do? I had thought that providing different levels of 'verbosity'
- * was the answer. I think this is useful to avoid being swamped in a sea of
- * shit but actually one needs at least an "instruction" as well as the "error".
- * 
- * There are at least two purposes to a msg: the problem and the action to take.
- * We need:
- * error codes which are complete
- * all possible information gathered.
- * adding and reinterpreting info at different levels.
- * be easy to extend as tw develops.
- * 
- * 
- * 
+ * messages are to interpret. But there are contexts and its a problem to write
+ * msg at a low level that have meaning as they bubble up to new contexts. So
+ * for any error these is the error and action.
  * 
  * Faced with a Graph: 3Worlds:projec1->DataDefinition:dataDef1, it seems better
  * to say:
  * 
- * "Add 'record' node to 'DataDefinition:dataDef1'" or
+ * "Add 'dataDefinition' node to '3worlds.prjt1'"
  * 
- * 'DataDefinition:dataDef1' requires 0..* children called 'record'
+ * instead of:
  * 
- * "Expected 0..* children of class 'record' for parents {DataDefinition,
- * Table}"
+ * "Expected 1..* children of class 'dataDefinition' for parents of{ 3worlds}"
+ * 
+ * So aims are: Have all msg strings in one place rather than dist. over many
+ * classes.
+ * 
+ * Allow modification / addition of messages in new context.
+ * 
+ * Allow different levels of detail (verbosity) so ultimately everything can be
+ * examined but also you can choose not to be overwhelmed with detail.
  */
 
-// Possibly goes in omhtk? Its really just for the construction and deployment of models from specifications etc etc
-// but it would be good if qgraph used it directly.
 public interface ErrorMessagable {
 	/* Brief description */
 	public String verbose1();
@@ -80,16 +65,15 @@ public interface ErrorMessagable {
 
 	/*
 	 * Allows some way of organizing msgs in some category order. Best used as a
-	 * prefix to all 'verbose<n> methods
+	 * prefix to all verbose1/2 strings
 	 */
 	public String category();
 
 	/*
 	 * Largely for debugging. Some string that indicates exactly the type of msg.
 	 * For example implementations may use enums and so the type could be the
-	 * enum.name(). Best used as a suffix to all verbose3() methods.
+	 * enum.name(). Additionl prefix to verbose2 strings
 	 */
 	public String errorName();
-
 
 }
