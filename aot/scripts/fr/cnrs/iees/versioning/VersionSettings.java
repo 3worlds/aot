cd58c210537343ea28da12300cc8cabfcdb732cb
Jacques Gignoux <jacques.gignoux@upmc.fr>
2021-05-20 16:40:12 +0200

    dependency consistency

--- aot/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ aot/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -74,7 +74,7 @@ public class VersionSettings {
 	 *
 	 */
 	protected static String[][] DEPS = {
-		{"fr.ens.biologie", "generics", "[0.2.3,)", null},
+		{"fr.ens.biologie", "generics", "[0.3.0,)", null},
 		{"au.edu.anu.rscs.aot", "omugi", "[0.4.3,)", null},
 		{"au.edu.anu.rscs.aot", "qgraph", "[0.1.4,)", null}
 	};
